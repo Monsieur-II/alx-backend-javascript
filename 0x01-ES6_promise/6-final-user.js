66d45f059370792652acf6da5d8a24b893431d17
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-04-23 19:47:20 +0000

    All tasks for promise project

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,2 +1,25 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise]).then((res) => {
+    const newArray = [];
+    res.forEach((data) => {
+      if (data.status === 'fulfilled') {
+        newArray.push({
+          status: data.status,
+          value: data.value,
+        });
+      } else {
+        newArray.push({
+          status: data.status,
+          value: `${data.reason}`,
+        });
+      }
+    });
+    return newArray;
+  });
+}
