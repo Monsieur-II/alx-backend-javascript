3be5a50e17b00ba9f465297d6c2fa76dd6a89f2e
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-04-23 18:43:57 +0000

    task 5

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,27 @@
+import uploadPhoto from './utils';
+import createUser from './utils';
+
+export function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+// export async function handleProfileSignup() {
+//   const photo = uploadPhoto();
+//   const user = createUser();
+
+//   try {
+//     const res = await Promise.all([photo, user]);
+//     console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+//   } catch {
+//     console.log('Signup system offline');
+//   }
+// }
