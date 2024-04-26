4fb62ef937a36da8180e3c01303ebbbcc1260ffa
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-04-26 05:38:02 +0000

    all

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -32,11 +32,11 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (
-      !Array.isArray(students) ||
-      !students.every((student) => typeof student === 'string')
-    ) {
+    if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array');
+    }
+    if (!students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
     return this._students;
   }
