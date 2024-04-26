89e47311d907b8c8532cbddab24f71849274a997
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-04-26 05:56:54 +0000

    update

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,46 +1,46 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(val) {
-    if (typeof val !== 'string') {
+  set name(name) {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = val;
+    this._name = name;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(val) {
-    if (typeof val !== 'number') {
+  set length(length) {
+    if (!Number.isInteger(length)) {
       throw new TypeError('Length must be a number');
     }
-    this._length = val;
+    this._length = length;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(val) {
-    if (!Array.isArray(val)) {
+  set students(students) {
+    if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array');
     }
     // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < val.length; i++) {
-      if (typeof val[i] !== 'string') {
+    for (let i = 0; i < students.length; i++) {
+      if (typeof students[i] !== 'string') {
         throw new TypeError('Students must be an array of strings');
       }
     }
-    this._students = val;
+    this._students = students;
   }
 }
