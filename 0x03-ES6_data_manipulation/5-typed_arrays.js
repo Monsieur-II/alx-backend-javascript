71a09c3a64b558f7e1d77671332bcee0d6307646
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-04-30 21:12:03 +0000

    mise a jour

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view[position] = value;
-  return view;
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const dataview = new DataView(new ArrayBuffer(length), 0, length);
+  dataview.setInt8(position, value);
+  return dataview;
 }
