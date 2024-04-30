630580771cf6561e91f36f688870e29765edb5d1
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-04-30 21:08:48 +0000

    data manipulation mandatory

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(groceries) {
+  if (!(groceries instanceof Map)) throw new Error('Cannot process');
+
+  for (const [key, value] of groceries) {
+    if (value === 1) {
+      groceries.set(key, 100);
+    }
+  }
+}
